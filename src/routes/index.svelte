915831dfce88cb1631c36f2dd5e77819ab8ad367
bielcapanema <capanema.ggfc@gmail.com>
2020-06-11 03:29:17 -0300

    roughjs

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,22 @@
+<script>
+	import Canvas from '../components/Canvas.svelte';
+</script>
 <style>
-	h1, figure, p {
+	h1 {
 		text-align: center;
 		margin: 0 auto;
-	}
-
-	h1 {
 		font-size: 2.8em;
 		text-transform: uppercase;
 		font-weight: 700;
 		margin: 0 0 0.5em 0;
 	}
 
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+	<title>Sveltedraw</title>
 </svelte:head>
 
-<h1>Great success2!</h1>
+<h1>Vrau</h1>
 
-<figure>
-	<img alt='Success Kid' src='successkid.jpg'>
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<Canvas />
