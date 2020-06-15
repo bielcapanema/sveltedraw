86e3595d3062bb0d788a244c34469f71c10f2e26
bielcapanema <capanema.ggfc@gmail.com>
2020-06-15 01:27:19 -0300

    canvas v1

---
+++ src/canvas/utils.js
@@ -0,0 +1,38 @@
+// If the element is created from right to left, the width is going to be negative
+// This set of functions retrieves the absolute position of the 4 points.
+// We can't just always normalize it since we need to remember the fact that an arrow
+// is pointing left or right.
+export function getElementAbsoluteX1(element) {
+  return element.width >= 0 ? element.x : element.x + element.width;
+}
+export function getElementAbsoluteX2(element) {
+  return element.width >= 0 ? element.x + element.width : element.x;
+}
+export function getElementAbsoluteY1(element) {
+  return element.height >= 0 ? element.y : element.y + element.height;
+}
+export function getElementAbsoluteY2(element) {
+  return element.height >= 0 ? element.y + element.height : element.y;
+}
+
+export function newElement(type, x, y) {
+  const element = {
+    type: type,
+    x: x,
+    y: y,
+    width: 0,
+    height: 0,
+    isSelected: false
+  };
+  return element;
+}
+
+export function rotate(x1, y1, x2, y2, angle) {
+  // 𝑎′𝑥=(𝑎𝑥−𝑐𝑥)cos𝜃−(𝑎𝑦−𝑐𝑦)sin𝜃+𝑐𝑥
+  // 𝑎′𝑦=(𝑎𝑥−𝑐𝑥)sin𝜃+(𝑎𝑦−𝑐𝑦)cos𝜃+𝑐𝑦.
+  // https://math.stackexchange.com/questions/2204520/how-do-i-rotate-a-line-segment-in-a-specific-point-on-the-line
+  return [
+    (x1 - x2) * Math.cos(angle) - (y1 - y2) * Math.sin(angle) + x2,
+    (x1 - x2) * Math.sin(angle) + (y1 - y2) * Math.cos(angle) + y2
+  ];
+}
--- src/components/Canvas.svelte
+++ src/components/Canvas.svelte
@@ -1,98 +1,280 @@
 <script>
+  import rough from "roughjs/bin/rough";
   import { onMount } from 'svelte';
+  import {
+    getElementAbsoluteX1,
+    getElementAbsoluteX2,
+    getElementAbsoluteY1,
+    getElementAbsoluteY2,
+    newElement,
+    rotate,
+  } from '../canvas/utils.js'
 
-  let canvas,
-    ctx,
-    flag = false,
-    prevX = 0,
-    currX = 0,
-    prevY = 0,
-    currY = 0,
-    dot_flag = false,
-    color = "black",
-    lineWidth = 2,
-    width = 400,
-    height = 400;
+  let handleDrawing,
+    screenWidth,
+    screenHeight,
+    context,
+    canvas,
+    generator,
+    rc;
 
-  function init() {
-    canvas = document.getElementById('canvas');
-    ctx = canvas.getContext("2d");
-    width = canvas.width;
-    height = canvas.height;
-
-    canvas.addEventListener("mousemove", function (e) {
-      findxy('move', e)
-    }, false);
-    canvas.addEventListener("mousedown", function (e) {
-      findxy('down', e)
-    }, false);
-    canvas.addEventListener("mouseup", function (e) {
-      findxy('up', e)
-    }, false);
-    canvas.addEventListener("mouseout", function (e) {
-      findxy('out', e)
-    }, false);
-  }
-    
-  function draw() {
-    ctx.beginPath();
-    ctx.moveTo(prevX, prevY);
-    ctx.lineTo(currX, currY);
-    ctx.strokeStyle = color;
-    ctx.lineWidth = lineWidth;
-    ctx.stroke();
-    ctx.closePath();
-  }
-  
-  function erase() {
-    var m = confirm("Want to clear");
-    if (m) {
-      ctx.clearRect(0, 0, width, height);
-    }
-  }
-  
-  function findxy(res, e) {
-    if (res == 'down') {
-      prevX = currX;
-      prevY = currY;
-      currX = e.clientX - canvas.offsetLeft;
-      currY = e.clientY - canvas.offsetTop;
-
-      flag = true;
-      dot_flag = true;
-      if (dot_flag) {
-        ctx.beginPath();
-        ctx.fillStyle = color;
-        ctx.fillRect(currX, currY, 2, 2);
-        ctx.closePath();
-        dot_flag = false;
-      }
-    }
-    if (res == 'up' || res == "out") {
-      flag = false;
-    }
-    if (res == 'move') {
-      if (flag) {
-        prevX = currX;
-        prevY = currY;
-        currX = e.clientX - canvas.offsetLeft;
-        currY = e.clientY - canvas.offsetTop;
-        draw();
-      }
-    }
-  }
+  let draggingElement = null;
+  let elementType = "selection";
 
   onMount(() => {
-    init();
-  })
+    screenWidth = window.innerWidth;
+    screenHeight = window.innerHeight;
+
+    let elements = [];
+    
+    canvas = document.getElementById("canvas");
+    rc = rough.canvas(canvas);
+    context = canvas.getContext("2d");
+    generator = rough.generator();
+
+    function clearSelection() {
+      elements.forEach(element => {
+        element.isSelected = false;
+      });
+    }
+
+    function setSelection(selection) {
+      const selectionX1 = getElementAbsoluteX1(selection);
+      const selectionX2 = getElementAbsoluteX2(selection);
+      const selectionY1 = getElementAbsoluteY1(selection);
+      const selectionY2 = getElementAbsoluteY2(selection);
+      elements.forEach(element => {
+        const elementX1 = getElementAbsoluteX1(element);
+        const elementX2 = getElementAbsoluteX2(element);
+        const elementY1 = getElementAbsoluteY1(element);
+        const elementY2 = getElementAbsoluteY2(element);
+        element.isSelected =
+          element.type !== "selection" &&
+          selectionX1 <= elementX1 &&
+          selectionY1 <= elementY1 &&
+          selectionX2 >= elementX2 &&
+          selectionY2 >= elementY2;
+      });
+    }
+
+    function generateDraw(element) {
+      if (element.type === "selection") {
+        element.draw = (rc, context) => {
+          const fillStyle = context.fillStyle;
+          context.fillStyle = "rgba(0, 0, 255, 0.10)";
+          context.fillRect(element.x, element.y, element.width, element.height);
+          context.fillStyle = fillStyle;
+        };
+      } else if (element.type === "rectangle") {
+        const shape = generator.rectangle(0, 0, element.width, element.height);
+        element.draw = (rc, context) => {
+          context.translate(element.x, element.y);
+          rc.draw(shape);
+          context.translate(-element.x, -element.y);
+        };
+      } else if (element.type === "ellipse") {
+        const shape = generator.ellipse(
+          element.width / 2,
+          element.height / 2,
+          element.width,
+          element.height
+        );
+        element.draw = (rc, context) => {
+          context.translate(element.x, element.y);
+          rc.draw(shape);
+          context.translate(-element.x, -element.y);
+        };
+      } else if (element.type === "arrow") {
+        const x1 = 0;
+        const y1 = 0;
+        const x2 = element.width;
+        const y2 = element.height;
+
+        const size = 30; // pixels
+        const distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        // Scale down the arrow until we hit a certain size so that it doesn't look weird
+        const minSize = Math.min(size, distance / 2);
+        const xs = x2 - ((x2 - x1) / distance) * minSize;
+        const ys = y2 - ((y2 - y1) / distance) * minSize;
+
+        const angle = 20; // degrees
+        const [x3, y3] = rotate(xs, ys, x2, y2, (-angle * Math.PI) / 180);
+        const [x4, y4] = rotate(xs, ys, x2, y2, (angle * Math.PI) / 180);
+
+        const shapes = [
+          //    \
+          generator.line(x3, y3, x2, y2),
+          // -----
+          generator.line(x1, y1, x2, y2),
+          //    /
+          generator.line(x4, y4, x2, y2)
+        ];
+
+        element.draw = (rc, context) => {
+          context.translate(element.x, element.y);
+          shapes.forEach(shape => rc.draw(shape));
+          context.translate(-element.x, -element.y);
+        };
+        return;
+      } else if (element.type === "text") {
+        element.draw = (rc, context) => {
+          const font = context.font;
+          context.font = element.font;
+          context.fillText(
+            element.text,
+            element.x,
+            element.y + element.measure.actualBoundingBoxAscent
+          );
+          context.font = font;
+        };
+      } else {
+        throw new Error("Unimplemented type " + element.type);
+      }
+    }
+    
+    function drawScene() {
+      context.clearRect(-0.5, -0.5, canvas.width, canvas.height);
+
+      elements.forEach(element => {
+        element.draw(rc, context);
+        if (element.isSelected) {
+          const margin = 4;
+
+          const elementX1 = getElementAbsoluteX1(element);
+          const elementX2 = getElementAbsoluteX2(element);
+          const elementY1 = getElementAbsoluteY1(element);
+          const elementY2 = getElementAbsoluteY2(element);
+          const lineDash = context.getLineDash();
+          context.setLineDash([8, 4]);
+          context.strokeRect(
+            elementX1 - margin,
+            elementY1 - margin,
+            elementX2 - elementX1 + margin * 2,
+            elementY2 - elementY1 + margin * 2
+          );
+          context.setLineDash(lineDash);
+        }
+      });
+    }
+
+    handleDrawing = (e) => {
+      const x = e.clientX - e.target.offsetLeft;
+      const y = e.clientY - e.target.offsetTop;
+      const element = newElement(elementType, x, y);
+
+      if (elementType === "text") {
+        const text = prompt("What text do you want?");
+        if (text === null) {
+          return;
+        }
+        element.text = text;
+        element.font = "20px Virgil";
+        const font = context.font;
+        context.font = element.font;
+        element.measure = context.measureText(element.text);
+        context.font = font;
+        const height =
+          element.measure.actualBoundingBoxAscent +
+          element.measure.actualBoundingBoxDescent;
+        // Center the text
+        element.x -= element.measure.width / 2;
+        element.y -= element.measure.actualBoundingBoxAscent;
+        element.width = element.measure.width;
+        element.height = height;
+      }
+
+      generateDraw(element);
+      elements.push(element);
+      if (elementType === "text") {
+        draggingElement = null;
+        elementType = "selection";
+        element.isSelected = true;
+      } else {
+        draggingElement = element;
+        // elementType = undefined;
+      }
+
+      const onMouseMove = (e) => {
+        // It is very important to read this.state within each move event,
+        // otherwise we would read a stale one!
+        const draggingElementTemp = draggingElement;
+        if (!draggingElementTemp) return;
+        let width = e.clientX - e.target.offsetLeft - draggingElementTemp.x;
+        let height = e.clientY - e.target.offsetTop - draggingElementTemp.y;
+        draggingElementTemp.width = width;
+        // Make a perfect square or circle when shift is enabled
+        draggingElementTemp.height = e.shiftKey ? width : height;
+
+        generateDraw(draggingElementTemp);
+
+        if (elementType === "selection") {
+          setSelection(draggingElementTemp);
+        }
+        drawScene();
+      };
+
+      const onMouseUp = e => {
+        window.removeEventListener("mousemove", onMouseMove);
+        window.removeEventListener("mouseup", onMouseUp);
+
+        const draggingElementTemp = draggingElement;
+        if (draggingElementTemp === null) {
+          return;
+        }
+        if (elementType === "selection") {
+          // Remove actual selection element
+          elements.pop();
+          setSelection(draggingElementTemp);
+        } else {
+          draggingElementTemp.isSelected = true;
+        }
+
+        draggingElement = null;
+        elementType = "selection";
+
+        drawScene();
+      };
+
+      window.addEventListener("mousemove", onMouseMove);
+      window.addEventListener("mouseup", onMouseUp);
+
+      drawScene();
+    }
+
+    canvas.addEventListener('mousedown', handleDrawing)
+  });
+
+  const menuItems = [
+    { value: "rectangle", children: "Rectangle" },
+    { value: "ellipse", children: "Ellipse" },
+    { value: "arrow", children: "Arrow" },
+    { value: "text", children: "Text" },
+    { value: "selection", children: "Selection" },
+  ];
 </script>
 
+<div class="menu">
+  {#each menuItems as menuItem (menuItem.value)}
+    <label>
+      <input
+        type="radio"
+        value={menuItem.value}
+        bind:group={elementType}
+      />
+      {menuItem.children}
+    </label>
+  {/each}
+</div>
+
+<canvas
+  id="canvas"
+  width={screenWidth}
+  height={screenHeight}
+/>
+
 <style>
-  canvas {
-    border: 2px solid;
+  .menu {
     margin: 0 auto;
-    display: block;
+    text-align: center;
   }
 </style>
-
-<canvas id="canvas" width="400" height="400"></canvas>
